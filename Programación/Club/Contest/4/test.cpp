b4cc6f7ecc963193673074725f0add25d904328e
akotadi <manuel_7795@hotmail.com>
2019-03-21 13:39:58 -0600

    Removing programation folder

--- Programación/Club/Contest/4/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-int main(){
-	int var = 14;
-	cout << (var&(~(1<<3))) << endl;
-	return 0;
-}
--- Programación/Dummies/SeparateModule.h
+++
@@ -1,3 +0,0 @@
-//SeparateModule.h - include the prototype declarations for functions contained within SeparateModule.cpp
-
-int sumSequence(ostream& out, istream& in);
